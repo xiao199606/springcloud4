f22c3e25622333a9872d02129566e11ec3b9224a
zhf12294 <2824592324@qq.com>
2019-09-18 09:15:39 +0800

    赵浩帆 初次修改

--- springcloud-provider-zhf/src/test/java/com/jk/SpringcloudProviderzhfApplicationTests.java
+++ springcloud-provider-zhf/src/test/java/com/jk/SpringcloudProviderzhfApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringcloudProviderLmhApplicationTests {
+public class SpringcloudProviderzhfApplicationTests {
 
     @Test
     public void contextLoads() {
