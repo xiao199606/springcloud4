ae1cadaf972f5283fe23f4ad91db1f60e881cef3
liminghao1127 <l15036771517@163.com>
2019-09-18 17:48:01 +0800

    发送邮件

--- springcloud-feign/src/main/java/com/jk/controller/LmhController.java
+++ springcloud-feign/src/main/java/com/jk/controller/LmhController.java
@@ -1,4 +1,28 @@
 package com.jk.controller;
 
+import com.jk.service.LmhServiceApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("lmh")
 public class LmhController {
+
+    @Autowired
+    private LmhServiceApi lmhServiceApi;
+    @PostMapping("/sendMails")
+    public Boolean sendMails() {
+
+        try {
+            lmhServiceApi.sendMails();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
 }
