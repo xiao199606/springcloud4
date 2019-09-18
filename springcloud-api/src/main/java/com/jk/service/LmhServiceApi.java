8c32e7cba17edcdeef9cc175f1f7af9afe7896a0
liminghao1127 <l15036771517@163.com>
2019-09-18 19:06:50 +0800

    发送邮件

--- springcloud-api/src/main/java/com/jk/service/LmhServiceApi.java
+++ springcloud-api/src/main/java/com/jk/service/LmhServiceApi.java
@@ -1,4 +1,10 @@
 package com.jk.service;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 public interface LmhServiceApi {
+
+    @PostMapping("sendMails")
+    Boolean sendMails();
+
 }
