49508f410bc8504637b2cc90468ace898721cd14
zhf12294 <2824592324@qq.com>
2019-09-19 19:29:47 +0800

    ！！！！！！！

--- springcloud-api/src/main/java/com/jk/model/Zwjl.java
+++ springcloud-api/src/main/java/com/jk/model/Zwjl.java
@@ -10,18 +10,21 @@
  */
 package com.jk.model;
 
-<<<<<<< HEAD
+
 import lombok.Data;
-=======
+
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.format.annotation.DateTimeFormat;
->>>>>>> 9f27dcb8c3222979b42c9ed9a244c7cf3e68cb64
+
 
 import java.util.Date;
 
 /**
- * 〈一句话功能简述〉<br> 
+ * 〈一句话功能简述〉<br>
  * 〈〉
  *
  * @author 斌
@@ -29,8 +32,11 @@ import java.util.Date;
  * @since 1.0.0
  */
 @Data
+@Document(indexName = "work",type = "workaction",shards = 3)
 public class Zwjl {
+    @Id
     private Integer id;
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_max_word")
     private String zhiw;
     private String leib;
     private String diq;
@@ -46,13 +52,13 @@ public class Zwjl {
     private String lianxr;
     private String lianxtel;
     private String email;
-    private Date fdate;
-
-    //一下是联查所需字段
-    private String gongsmc;//公司名称
-    private String gongsxz;//公司性质
-
     @DateTimeFormat(pattern = "yyyy-MM-dd") // 处理从	前端到后端的时间
     @JsonFormat(pattern = "yyyy-MM-dd",timezone="GMT+8")// 处理从	后端到前端的时间
     private Date fdate;
+    //一下是联查所需字段
+    private String gongsmc;//公司名称
+    private String gongsxz;//公司性质
+    private Integer minsalary;//小薪资区间
+    private Integer maxsalary;//大薪资区间
+    private String yuangrs;
 }
