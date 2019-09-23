9c7aafa0a1e5fe460ebfdb33075b1048d5074968
liminghao1127 <l15036771517@163.com>
2019-09-23 08:51:26 +0800

    Excel导出

--- springcloud-api/src/main/java/com/jk/service/LmhServiceApi.java
+++ springcloud-api/src/main/java/com/jk/service/LmhServiceApi.java
@@ -1,6 +1,10 @@
 package com.jk.service;
 
+import com.jk.model.JianLi;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface LmhServiceApi {
 
@@ -10,4 +14,7 @@ public interface LmhServiceApi {
     Boolean failureMails();
     @PostMapping
     void updateJianLi();
+
+    @PostMapping("queryJianL")
+    List<JianLi> queryJianL(@RequestParam(value = "ids1",required = true) Integer ids1);
 }
